e0f9024dfb652b9275f51a2c7dbb6f279e95483e
hongq <hq3009@gmail.com>
2022-09-28 21:36:39 +0800

    2022-09-28

--- src/yinzhou_fund.py
+++ src/yinzhou_fund.py
@@ -2,12 +2,17 @@
 import pandas as pd
 
 # %%
-yinzhou_fund = pd.read_excel('../data/yinzhou/fund.xlsx')
+yinzhou_fund = pd.read_excel('../data/yinzhou/yinzhou_fund_raw.xlsx')
 
 # %%
-yinzhou_fund_equity = yinzhou_fund[~yinzhou_fund.business.str.contains("私募证券投资基金")]
-yinzhou_fund_equity = yinzhou_fund_equity[yinzhou_fund_equity.full_name.str.contains("（有限合伙）")]
+yinzhou_fund_equity = yinzhou_fund[~yinzhou_fund.business.str.
+                                   contains("私募证券投资基金")]
+yinzhou_fund_equity = yinzhou_fund_equity[
+    yinzhou_fund_equity.full_name.str.contains("（有限合伙）")]
+
+yinzhou_fund_equity_survival = yinzhou_fund_equity[yinzhou_fund_equity.status
+                                                   == "存续"]
 
 # %%
-name_list = yinzhou_fund_equity.full_name
-name_list = name_list.drop_duplicates(keep='first')
+name_list = yinzhou_fund_equity_survival.legal_representive
+name_list_clean = name_list.drop_duplicates()
